5936780920f458048ad79ad9c276fc5560637795
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-01 04:46:12 +0800

    Toggle show/hide more projects on more btn click
    Show and hide functions were made in more-projects.js. The display style
    on more projects is toggled to either flex or none to simulate a show
    and hide it.

--- js/more-projects.js
+++ js/more-projects.js
@@ -1,23 +1,36 @@
 const moreButton = document.querySelector(".more-btn");
 const moreIcon = document.querySelector(".more-icon");
 const moreButtonText = document.querySelector(".more-icon + p");
+const moreProjects = document.querySelector(".more-projects");
 
 console.log(moreButton, moreIcon);
 
 moreButton.addEventListener("click", () => {
   console.log(moreIcon.classList.contains("fa-angle-down"));
   if (moreIcon.classList.contains("fa-angle-down")) {
-    changeIcon(moreIcon, "fa-angle-down", "fa-angle-up");
-    moreButtonText.innerHTML = "See Less";
+    showMore();
   }
   else {
-    changeIcon(moreIcon, "fa-angle-up", "fa-angle-down");
-    moreButtonText.innerHTML = "See More";
+    showLess();
   }
 });
+
+function showMore() {
+  moreProjects.style.display = "flex";
+  changeIcon(moreIcon, "fa-angle-down", "fa-angle-up");
+  moreButtonText.innerHTML = "See Less";
+}
+
+function showLess() {
+  moreProjects.style.display = "none";
+  changeIcon(moreIcon, "fa-angle-up", "fa-angle-down");
+  moreButtonText.innerHTML = "See More";
+
+}
 
 
 function changeIcon(icon, oldClass, newClass) {
   icon.classList.remove(oldClass);
   icon.classList.add(newClass);
 }
+
