469a769b0059d01ea286c9b12547e38fd24eb1bc
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-03 00:50:21 +0800

    Add a light mode color theme
    Toggle color theme button works.

---
+++ js/toggle-color-theme.js
@@ -0,0 +1,24 @@
+const colorThemeButton = document.querySelector(".toggle-theme-btn");
+const colorThemeIcon = document.querySelector(".toggle-theme-btn > i")
+
+
+colorThemeButton.addEventListener("click", () => {
+  toggleColorThemeIcon();
+  toggleColorTheme();
+});
+
+function toggleColorTheme() {
+  document.documentElement.classList.toggle("light");
+  // document.documentElement.classList.toggle("fa-sun");
+}
+
+function toggleColorThemeIcon() {
+  if (colorThemeIcon.classList.contains("fa-sun")) {
+      colorThemeIcon.classList.remove("fa-sun");
+      colorThemeIcon.classList.add("fa-moon");
+    }
+    else {
+    colorThemeIcon.classList.remove("fa-moon");
+    colorThemeIcon.classList.add("fa-sun");
+  }
+}
