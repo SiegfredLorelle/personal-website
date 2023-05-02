3fbbec06e8e498ace0d86e7608a1b5dfa5d2be44
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-02 19:49:54 +0800

    Fix html semantics
    use h1 tag for 'Siegfred Mina' at home section. Encapsulate the h1 tag
    with p tags for description and name introduction.

--- js/trigger-nav.js
+++ js/trigger-nav.js
@@ -1,43 +1,3 @@
-// const body = document.querySelector("body");
-// const hamburgerBtn = document.querySelector(".top-nav-hamburger-btn");
-// const nav = document.querySelector(".top-nav-list.mobile");
-// const sections = document.querySelectorAll("section");
-
-
-// function showNav() {
-//   nav.style.display = "flex";
-//   sections.forEach(section => {
-//     section.style.opacity = "0.2";
-//   })
-// }
-// function hideNav() {
-//   nav.style.display = "none";
-//   sections.forEach(section => {
-//     section.style.opacity = "1";
-//   })
-// }
-
-// function toggleNav() {
-//   if (nav.style.display == "flex") {
-//     hideNav();
-//   }
-//   else {
-//     showNav();
-//   }
-// }
-
-// hamburgerBtn.addEventListener("click", toggleNav)
-
-
-
-// sections.forEach(section => {
-//   section.addEventListener("click", () => {
-  //     hideNav();
-  //   })
-  // })
-
-
-
 const topNav = document.querySelector(".top-nav-list");
 const topNavItems = document.querySelectorAll(".top-nav-list > a");
 
@@ -47,8 +7,6 @@ const beforeTopNavMobile = document.querySelector("header");
 const hamburgerBtn = document.querySelector(".top-nav-hamburger-btn");
 const main = document.querySelector("main");
 
-
-// console.log(topNav, beforeTopNavDesktop, beforeTopNavMobile);
 
 hamburgerBtn.addEventListener("click", toggleNav);
 
